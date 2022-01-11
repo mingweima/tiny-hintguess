a6aa5204c52ed7a53a9f295d855818cef16f9bba
Mingwei Ma <mma3@chicagobooth.edu>
2022-01-11 15:09:34 -0600

    Major updates - change path

---
+++ source/agent.py
@@ -0,0 +1,136 @@
+from abc import ABC, abstractmethod
+from copy import deepcopy
+import sys
+
+from model import *
+
+
+class Agent(ABC):
+    def __init__(self, ndim, hsize):
+        self.ndim = ndim
+        self.hsize = hsize
+
+    @abstractmethod
+    def update_rates(self, denominator: float) -> None:
+        """Linearly decay learning (and exploration) rates
+        Args:
+            denominator: Timescale of decay
+        """
+
+
+class DQNAgent(Agent):
+    def __init__(self, env, ndim=10, hsize=4, agent_config=None):
+        super().__init__(ndim, hsize)
+        if agent_config is None:
+            agent_config = {}
+        agent_config.setdefault('embedding_dim', 12)
+        agent_config.setdefault('action_space_size', hsize)
+        agent_config.setdefault('policy_type', 'ActionIn')
+        agent_config.setdefault('replay_capacity', int(1e5))
+        agent_config.setdefault('learning_rate', 1e-4)
+        agent_config.setdefault('batch_size', 500)
+        agent_config.setdefault('eps_start', 1.)
+        agent_config.setdefault('eps_end', 0.05)
+        agent_config.setdefault('eps_decay', int(1e5))
+        agent_config.setdefault('encoding_function', 'one_hot')
+        agent_config.setdefault('num_head', 5)
+        self.env = env
+        self.agent_config = agent_config
+        self.epsilon = agent_config['eps_start']
+        self.steps_done = 0
+        self.memory = ReplayMemory(self.agent_config['replay_capacity'])
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        seq_len, embedding_dim = 2 * hsize + 1, self.agent_config['embedding_dim']
+
+        # Encoding function: support one hot or sin wave
+        if agent_config['encoding_function'] == 'one_hot':
+            self.encoding_function = one_hot_encoding
+        elif agent_config['encoding_function'] == 'sin':
+            self.encoding_function = sin_positional_encoding
+        else:
+            raise ValueError("Invalid encoding function!")
+
+        # Policy net: input tensor of size (B: batch size, N: sequence len, D: embed dim)
+        # output tensor of size (B, num_actions) containing Q values
+        if agent_config['policy_type'] == "ActionIn":
+            self.policy_net = ActionInModel(seq_len, embedding_dim, num_head=agent_config['num_head'])
+        elif agent_config['policy_type'] == "DQN":
+            self.policy_net = DQNModel(seq_len, embedding_dim)
+        else:
+            raise ValueError("Given policy type invalid!")
+        self.policy_net.to(self.device)
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr=agent_config['learning_rate'])
+
+    def __str__(self) -> str:
+        return self.agent_config['policy_type'] + '-' + self.agent_config['encoding_function'] + '-' + str(
+            self.env.ndim) + '-' + str(self.env.hsize)
+
+    def detach_copy(self) -> Agent:
+        """save model snapshot that only contains NN params but no memory"""
+        new_agent = self.__class__(self.env, ndim=self.ndim, hsize=self.hsize, agent_config=self.agent_config)
+        new_agent.policy_net = deepcopy(self.policy_net)
+        new_agent.optimizer = torch.optim.Adam(new_agent.policy_net.parameters(), lr=self.agent_config['learning_rate'])
+        return new_agent
+
+    def update_rates(self, denominator: float) -> None:
+        """update epsilon for agent's exploration"""
+        self.epsilon = self.agent_config['eps_end'] + (
+                self.agent_config['eps_start'] - self.agent_config['eps_end']) * \
+                       math.exp(-1. * denominator / self.agent_config['eps_decay'])
+
+    def transform_obs(self, obs: torch.Tensor) -> torch.Tensor:
+        """transform obs from array of integers to encodings"""
+        # The obs is (h1, h2, target/hint), which are all 1D arrays
+        return self.encoding_function(obs, d_model=self.agent_config[
+            'embedding_dim'])  # N*d, where N is the number of cards, d is the embedding dim
+
+    def select_action(self, obs: torch.Tensor, evaluate=False) -> torch.Tensor:
+        """given obs, select action for agent"""
+        obs = self.transform_obs(obs)  # now obs is N*d
+        obs = obs.unsqueeze(0)  # (1, N, D) for batch size = 1
+        sample = random.random()
+        if evaluate:
+            return self.policy_net(obs).max(1)[1].view(1, 1)
+        self.steps_done += 1
+        self.update_rates(self.steps_done)
+        # print(sample, self.epsilon)
+        if sample > self.epsilon:
+            with torch.no_grad():
+                # t.max(1) will return largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self.policy_net(obs).max(1)[1].view(1, 1)
+        else:
+            return torch.tensor([[random.randrange(self.agent_config['action_space_size'])]], device=self.device,
+                                dtype=torch.long)
+
+    def optimize_model(self) -> (float, float, float):
+        """one step gradient update"""
+        if len(self.memory) < self.agent_config['batch_size']:
+            return
+        transitions = self.memory.sample(self.agent_config['batch_size'])
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+        state_batch = torch.stack(batch.state, dim=0)  # (bs, embdim, numcards)
+        # print(state_batch.size())
+        state_batch = torch.einsum('ben->bne', state_batch)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch)
+        state_action_values = state_action_values.gather(1, action_batch)
+        expected_state_action_values = reward_batch
+        criterion = nn.MSELoss()
+        # criterion = nn.HuberLoss()
+        loss = criterion(state_action_values.squeeze(1), expected_state_action_values)
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item(), expected_state_action_values.mean().item(), state_action_values.mean().item()
---
+++ source/train_agents.py
@@ -0,0 +1,142 @@
+import _pickle as cPickle
+import argparse
+import hashlib
+import os
+import sys
+import time
+from pathlib import Path
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+import torch
+import yaml
+
+from agent import DQNAgent
+from game import TinyHintGuessGame
+
+sys.path.append(os.getcwd())
+sys.path.append("")
+
+# if gpu is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+parser = argparse.ArgumentParser(description='Specify the directory of the config file!')
+parser.add_argument(
+    "--config-file",
+    default="../configs/foo.yaml",
+    metavar="FILE",
+    help="path to config file",
+    type=str,
+)
+args = parser.parse_args()
+
+with open(args.config_file, encoding='utf-8') as f:
+    usr_config = yaml.safe_load(f)
+
+print(torch.__version__)
+
+
+def train_agents(verbose: bool = True, config: Optional[dict] = None):
+    """train agents using configs and pickle results"""
+    if config is None:
+        config = {}
+    # unpack config
+    num_episodes = config.get("num_episodes", int(3e6))
+    ndim, hsize = config.get("ndim", 5), config.get("hsize", 3)
+    update_frequency = config.get("update_frequency", 250)
+    agent_class = config.get("agent_class", DQNAgent)
+    agent_config = config.get("agent_config", {})
+
+    env = TinyHintGuessGame(ndim=ndim, hsize=hsize)
+    hinter = agent_class(env, ndim=ndim, hsize=hsize, agent_config=agent_config)
+    guesser = agent_class(env, ndim=ndim, hsize=hsize, agent_config=agent_config)
+
+    running_rewards = []
+
+    # calculate time hash for saving models and create str for agent
+    print("Training started with configs:")
+    print(config)
+    hash_time = hashlib.sha1()
+    hash_time.update(str(time.time()).encode('utf-8'))
+    hash_time = hash_time.hexdigest()[:6]
+    agent_str = str(hinter)
+
+    # create path and save yaml file
+    save_path = f"../{config['result_dir']}/{agent_str}-{hash_time}"
+    Path(save_path).mkdir(parents=True, exist_ok=True)
+    with open(f"{save_path}/configs.yaml", 'w') as handle:
+        yaml.dump(config, handle)
+
+    for i_episode in range(num_episodes):
+        # run single episode
+        obs_to_hinter = env.reset()
+        hint_action = hinter.select_action(torch.tensor(obs_to_hinter, device=device))
+        obs_to_guesser, _, _, _ = env.step(hint_action.item())
+        guess_action = guesser.select_action(torch.tensor(obs_to_guesser, device=device))
+        _, r, _, _ = env.step(guess_action)
+
+        # store the transition in memory
+        obs_to_hinter = torch.tensor(np.array(obs_to_hinter), device=device)
+        obs_to_guesser = torch.tensor(np.array(obs_to_guesser), device=device)
+        hint_action = torch.tensor(np.array(hint_action), device=device)
+        guess_action = torch.tensor(np.array(guess_action), device=device)
+        r = torch.tensor([r], device=device)
+        hinter.memory.push(hinter.transform_obs(obs_to_hinter).T, hint_action, None, r)
+        guesser.memory.push(guesser.transform_obs(obs_to_guesser).T, guess_action, None, r)
+
+        # perform one step of the optimization (on the policy network) every update_frequency steps
+        if i_episode % update_frequency == 0:
+            res1 = hinter.optimize_model()
+            res2 = guesser.optimize_model()
+            if res1:
+                hloss, hq, hqhat = res1
+                gloss, gq, gqhat = res2
+
+        # append rewards to running rewards and print automatically
+        running_rewards.append(r.cpu().numpy()[0])
+        print_num = max(num_episodes // 500, 10)
+        save_num = max(num_episodes // 10, 100)
+        if i_episode > 1 and i_episode % print_num == 0:
+            rw_to_print = np.array(running_rewards[-print_num:])
+            running_rewards = []
+            mean_win = np.sum(np.array(rw_to_print) >= 0, axis=0) / rw_to_print.shape[0]
+            localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+            # logging and printing
+            log_csv = f"{save_path}/log.csv"
+            log_df = pd.DataFrame([[i_episode, mean_win, hloss, gloss, hq, hqhat, gq, gqhat, hinter.epsilon]],
+                                  columns=['episodes', 'return', 'hinter_loss', 'guesser_loss',
+                                           'hinter_q', 'hinter_qhat', 'guesser_q', 'guesser_qhat',
+                                           'exploration'])
+            log_csv_exists = os.path.isfile(log_csv)
+            use_header = not log_csv_exists
+            log_df.to_csv(log_csv,
+                          index=False,
+                          header=use_header,
+                          mode='a',  # append data to csv file
+                          chunksize=1)  # size of data to append for each loop
+            if verbose:
+                print(localtime, i_episode, mean_win, hinter.epsilon)
+                print(round(hloss, 2), round(hq, 2), round(hqhat, 2), round(gloss, 2), round(gq, 2), round(gqhat, 2))
+
+        if i_episode > 1 and i_episode % save_num == 0:
+            # save model snapshots periodically
+            hinter_snapshot = hinter.detach_copy()
+            guesser_snapshot = guesser.detach_copy()
+            with open(f"{save_path}/{i_episode}.pkl", "wb") as output_file:
+                cPickle.dump({'p1': hinter_snapshot, 'p2': guesser_snapshot}, output_file)
+            print(f"Snapshot {i_episode} saved at {save_path}.")
+
+    hinter.memory = None
+    guesser.memory = None
+    res = {'p1': hinter, 'p2': guesser}
+    with open(f"{save_path}/{num_episodes}.pkl", "wb") as output_file:
+        cPickle.dump({'p1': hinter, 'p2': guesser}, output_file)
+    print(f"Final result saved at {save_path}.")
+    return res
+
+
+if __name__ == '__main__':
+    res_dict = train_agents(config=usr_config, verbose=False)
+    print(usr_config)
